56695689ab7f6588065eab9d883db843b082f873
GuyYaffeErmoza <guyyaffear@gmail.com>
2020-07-30 08:34:03 +0300

    adding Geo API

---
+++ seasons/src/SeasonDisplay.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const SeasonDisplay =() =>{
+    return (
+        <div>Season</div>
+    )
+};
+
+ReactDOM.render(
+    <SeasonDisplay />,
+    document.querySelector('#root')
+)
